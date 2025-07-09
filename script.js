7d8b2d0bf66cbc75e16f82f8b4a9c7c775028be1
Marcel Härle <marcel.haerle@sonnvest.de>
2025-07-09 12:16:23 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector("#lightBtn").addEventListener("click", () => {
+  document.body.className = "light";
+});
+
+document.querySelector("#darkBtn").addEventListener("click", () => {
+  document.body.className = "dark";
+});
+
+document.querySelector("#partyBtn").addEventListener("click", () => {
+  document.body.className = "party";
+});
